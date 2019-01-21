43380b1f18515aeb169d8786c3f23c490c411130
feriansyah <feriansyah.ms@gmail.com>
2019-01-21 19:19:34 +0800

    Adding GET /todos

--- server/server.js
+++ server/server.js
@@ -22,6 +22,13 @@ app.post('/todos', (req, res) => {
     })
 });
 
+app.get('/todos', (req, res) => {
+  Todo
+    .find()
+    .then(results => res.send({results}))
+    .catch(e => res.status(400).send(e));
+});
+
 app.listen(3000, () => {
   console.log('Server run in port 3000');
 });
