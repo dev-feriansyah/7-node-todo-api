39c9944adea11c50acf92d29cc88e111211fbd54
feriansyah <feriansyah.ms@gmail.com>
2019-01-21 19:35:46 +0800

    Add test for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,16 +4,23 @@ const expect  = require('expect'),
 const {app}  = require('./../server'),
       {Todo} = require('./../models/todo');
 
+const todosSeed = [
+  {text: 'First todo'},
+  {text: 'Second todo'}
+];
 
 beforeEach(done => {
-  Todo.deleteMany({}).then(() => done());
+  Todo
+    .deleteMany({})
+    .then(() => {
+      return Todo.insertMany(todosSeed);
+    })
+    .then(() => done());
 });
 
 describe('POST /todos', () => {
-
   it('should create todo document in database', done => {
     const text = 'Mocha testing todo';
-
     request(app)
       .post('/todos')
       .send({text})
@@ -25,18 +32,14 @@ describe('POST /todos', () => {
         if(err) {
           return done(err);
         }
-
-        Todo.find().then(todos => {
+        Todo.find({text}).then(todos => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
         }).catch(e => done(e));
-
       });
   });
-
   it('should NOT create todo and response with 400 Code', done => {
-
     request(app)
       .post('/todos')
       .send({})
@@ -45,14 +48,22 @@ describe('POST /todos', () => {
         if(err) {
           return done(err);
         }
-
         Todo.find().then(todos => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch(e => done(e));
-
-      })
-
+      });
   });
+});
 
+describe('GET /todos', () => {
+  it('should get all todos in database', done => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.results.length).toBe(2);
+      })
+      .end(done);
+  });
 });
