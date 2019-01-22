2a963c2ed00efc605190c20f5b414f815ccc400f
feriansyah <feriansyah.ms@gmail.com>
2019-01-22 19:37:33 +0800

    Setting up heroku app

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 // mongoose v5 using native promise as default
 // mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://192.168.1.1:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(PROD_MONGODB || 'mongodb://192.168.1.1:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ const {mongoose} = require('./db/mongoose');
 const {Todo}     = require('./models/todo');
 const {User}     = require('./models/user');
 
-const app = express();
+const app  = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -51,7 +52,7 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server run in port 3000');
 });
 
