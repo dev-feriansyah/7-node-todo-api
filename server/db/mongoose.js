78d20a1f5f2e0fad75851e1d030302a3ebe20dfc
feriansyah <feriansyah.ms@gmail.com>
2019-01-22 19:47:32 +0800

    fix mongoUrl script in db/mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // mongoose v5 using native promise as default
 // mongoose.Promise = global.Promise;
-const mongoUrl = PROD_MONGODB || 'mongodb://192.168.1.1:27017/TodoApp';
+const mongoUrl = process.env.PROD_MONGODB || 'mongodb://192.168.1.1:27017/TodoApp';
 mongoose.connect(mongoUrl, { useNewUrlParser: true });
 
 module.exports = {mongoose};
