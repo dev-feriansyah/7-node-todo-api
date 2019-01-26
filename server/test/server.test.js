e6d32598321782d3cef4c948e0444590fe04d37d
feriansyah <feriansyah.ms@gmail.com>
2019-01-26 18:11:39 +0800

    Add test cases for DELETE /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -291,3 +291,19 @@ describe('POST /users/login', () => {
       });
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should delete user token', done => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', seed.users[0].tokens[0].token)
+      .expect(200)
+      .end(err => {
+        if(err) return done(err);
+        User.findById(seed.users[0]._id).then(user => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch(e => done(e));
+      });
+  });
+});
